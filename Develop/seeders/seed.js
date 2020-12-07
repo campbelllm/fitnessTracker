b85655cf6cec4d91b0a61308c9156f4e8e5273f0
Lindsey Smith <campbelllm@gmail.com>
2020-12-07 15:13:14 -0700

    adding model, routes, and updating package json

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb+srv://campbelllm:password@cluster0.6oiwf.mongodb.net/test", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
