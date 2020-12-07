afe8f9537baba29e10ba56c309ecd01e9547fe06
Lindsey Smith <campbelllm@gmail.com>
2020-12-07 14:55:22 -0700

    created server

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+require('dotenv')
+  .config();
+const express = require('express');
+const mongoose = require('mongoose');
+// const routes = require('./routes');
+const path = require('path');
+// require('./services/passport');
+const app = express();
+const logger = require("morgan");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb+srv://campbelllm:password@cluster0.6oiwf.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Yee'))
+  .catch(e => console.log(e));
+
+mongoose.set('debug', true);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+app.use(routes);
+app.listen(PORT, () => {
+  console.log('Server started listening on PORT http://localhost:3000');
+});
